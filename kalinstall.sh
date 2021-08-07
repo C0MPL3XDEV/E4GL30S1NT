bef2394782d15f9d250e1585a7ee42181dc097b3
C0MPL3X <78054834+C0MPL3XDEV@users.noreply.github.com>
2021-08-07 11:49:27 +0200

    Update kalinstall.sh

--- kalinstall.sh
+++ kalinstall.sh
@@ -20,5 +20,5 @@ pip3 install email-validator
 echo -e "$b"">""$w"" installing modules: ""$g""googlesearch-python""$w"
 pip3 install googlesearch-python
 echo -e "$b"">""$w"" successfully installing dependencies"
-sudo wget -q https://raw.githubusercontent.com/termuxhackers-id/SIGIT/main/sigit.py -O /usr/local/bin/E4GL30S1NT && sudo chmod +x /usr/local/bin/E4GL30S1NT
+sudo wget -q https://raw.githubusercontent.com/C0MPL3XDEV/E4GL3OS1NT/main/E4GL30S1NT.py -O /usr/local/bin/E4GL30S1NT && sudo chmod +x /usr/local/bin/E4GL30S1NT
 echo -e "$b"">""$w"" use command ""$g""E4GL3OS1NT""$w"" for start the console"
