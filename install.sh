f9fec605d21c748d7c600585560387a3beb71274
C0MPL3XDEV <78054834+C0MPL3XDEV@users.noreply.github.com>
2021-09-21 17:24:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
-echo -e "$b"">""$w"" E4GL30SINT - Simple information gathering tool"
+echo -e "$b"">""$w"" E4GL30SINT - Simple information gathering toolkit"
 echo -e "$b"">""$w"" prepare for installing dependencies ..."
 sleep 3
 echo -e "$b"">""$w"" installing package: ""$g""libxml2""$w"
@@ -17,10 +17,10 @@ echo -e "$b"">""$w"" installing modules: ""$g""lxml""$w"
 pip3 install wheel lxml
 echo -e "$b"">""$w"" installing modules: ""$g""requests""$w"
 pip3 install requests
-echo -e "$b"">""$w"" installing modules: ""$g""email-validator""$w"
-pip3 install email-validator
-echo -e "$b"">""$w"" installing modules: ""$g""googlesearch-python""$w"
-pip3 install googlesearch-python
+echo -e "$b"">""$w"" installing modules: ""$g""BeautifulSoup""$w"
+pip3 install BeautifulSoup
+echo -e "$b"">""$w"" installing modules: ""$g""tabulate""$w"
+pip3 install tabulate
 echo -e "$b"">""$w"" successfully installing dependencies"
 wget -q https://raw.githubusercontent.com/C0MPL3XDEV/E4GL3OS1NT/main/E4GL30S1NT.py -O "$PREFIX"/bin/E4GL30S1NT && chmod +x "$PREFIX"/bin/E4GL30S1NT
 echo -e "$b"">""$w"" use command ""$g""E4GL30S1NT""$w"" for start the console"
