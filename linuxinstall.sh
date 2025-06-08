d54558575dd62470285dabae7a8c032d6768e515
jose guzman <32862241+joseguzman1337@users.noreply.github.com>
2025-06-07 21:49:02 -0500

    Update linuxinstall.sh

--- linuxinstall.sh
+++ linuxinstall.sh
@@ -9,9 +9,9 @@ echo -e "$b"">""$w"" prepare for installing dependencies ..."
 sleep 3
 echo -e "$b"">""$w"" installing package: ""$g""libxml2""$w"
 sudo apt install libxml2 -y
-echo -e "$b"">""$w"" installing pacakge: ""$g""libxslt""$w"
+echo -e "$b"">""$w"" installing package: ""$g""libxslt""$w"
 sudo apt install libxslt1.1 -y
-echo -e "$b"">""$w"" installing pacakge: ""$g""python3""$w"
+echo -e "$b"">""$w"" installing package: ""$g""python3""$w"
 sudo apt install python3 python-is-python3 -y
 echo -e "$b"">""$w"" installing package: ""$g""python3-pip""$w"
 sudo apt install python3-pip -y
@@ -23,6 +23,11 @@ echo -e "$b"">""$w"" installing modules: ""$g""BeautifulSoup""$w"
 sudo apt install python3-bs4 -y
 echo -e "$b"">""$w"" installing modules: ""$g""tabuate""$w"
 sudo apt install python3-tabulate -y
+echo -e "$b"">""$w"" installing modules: ""$g""pyperclip""$w"
+if ! sudo apt install python3-pyperclip -y; then
+    echo -e "$r""[!] apt install failed, trying pip3...""$w"
+    pip3 install pyperclip
+fi
 echo -e "$b"">""$w"" successfully installing dependencies"
 sudo wget -q https://raw.githubusercontent.com/C0MPL3XDEV/E4GL3OS1NT/main/E4GL30S1NT.py -O /usr/local/bin/E4GL30S1NT && sudo chmod +x /usr/local/bin/E4GL30S1NT
 echo -e "$b"">""$w"" use command ""$g""E4GL30S1NT""$w"" for start the console"
